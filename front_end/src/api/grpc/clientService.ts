70d3849bb10b5beda79b6aa2b4b820898ae8bfcc
GAO <gaoxiangshuai@gmail.com>
2024-09-04 20:21:21 -0400

    fixed proto errors idk how

--- front_end/src/api/grpc/clientService.ts
+++ front_end/src/api/grpc/clientService.ts
@@ -1,9 +1,9 @@
-import { UserClient } from "../../proto/Twitter_cloneServiceClientPb";
+import { UserClient } from "../../proto/twitter_clone.client";
 import { APP_URL } from "../../utils/constants";
 
 export class ClientService {
     private client: UserClient;
     constructor(){
-        // this.client = new UserClient(APP_URL);
+        this.client = new UserClient(APP_URL);
     }
 }
