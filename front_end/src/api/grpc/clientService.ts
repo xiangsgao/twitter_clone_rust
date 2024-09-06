dac21ef02dd2c89c0947548e8ff463a403f7d200
GAO <gaoxiangshuai@gmail.com>
2024-09-06 16:04:41 -0400

    updated

--- front_end/src/api/grpc/clientService.ts
+++ front_end/src/api/grpc/clientService.ts
@@ -1,9 +1,38 @@
+import { LoginUserRequest, RegisterUserRequest } from "../../proto/twitter_clone";
 import { UserClient } from "../../proto/twitter_clone.client";
 import { APP_URL } from "../../utils/constants";
 
 export class ClientService {
-    private client: UserClient;
+    private userClient: UserClient;
     constructor(){
-        this.client = new UserClient(APP_URL);
+        this.userClient = new UserClient(APP_URL);
+    }
+
+
+    loginUser(email: string, password: string){
+        const loginUserRequest: LoginUserRequest = {
+            email,
+            password
+        };
+        return this.userClient.loginUser(loginUserRequest);
+    }
+
+    logoutUser(){
+        return this.userClient.logoutUser({});
+    }
+
+    getUser(){
+        return this.userClient.getUser({});
+    }
+
+    registerUser(email: string, password: string, firstName: string, lastName: string){
+        const registerUserRequest: RegisterUserRequest = {
+            email,
+            password,
+            firstName,
+            lastName
+        }
+        return this.userClient.registerUser(registerUserRequest);
+      
     }
 }
