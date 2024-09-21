5a0c60e9d20b92a9932c0a415931d85ef1f44556
GAO <gaoxiangshuai@gmail.com>
2024-09-21 18:40:26 -0400

    updated

---
+++ front_end/src/components/Post/Post.tsx
@@ -0,0 +1,10 @@
+import { Card } from "antd";
+import { FC } from "react";
+
+export const Post: FC = () =>{
+    return (
+        <Card>
+            Tweet
+        </Card>
+    )
+}
--- front_end/src/views/Home/Home.tsx
+++ front_end/src/views/Home/Home.tsx
@@ -1,34 +1,17 @@
 import {Button} from "antd";
 import {useState} from "react";
 import {APP_URL} from "../../utils/constants.ts";
+import styled from "styled-components";
 
+const StyledDiv = styled.div`
 
+`
 const Home = () =>{
     const [count, setCount] = useState(0)
     return (
-        <>
-            <h1>{APP_URL}</h1>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={'/vite.svg'} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={"/assets/react.svg"} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React + Antd</h1>
-            <div className="card">
-                <Button onClick={() => setCount((count) => count + 1)}>
-                    count is {count} from antd button
-                </Button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <StyledDiv>
+            
+        </StyledDiv>
     )
 }
 
