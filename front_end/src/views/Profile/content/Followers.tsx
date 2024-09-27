1e1787555a740e3bde0bf898ae67a4d942161b87
GAO <gaoxiangshuai@gmail.com>
2024-09-27 19:17:16 -0400

    updated

--- front_end/src/views/Profile/content/Followers.tsx
+++ front_end/src/views/Profile/content/Followers.tsx
@@ -1,9 +1,17 @@
-import { List } from "antd"
+import { Avatar, Card, List } from "antd"
 
 export const Followers = () =>{
     return (
-        <List>
-            
-        </List>
+        <List 
+        itemLayout="horizontal"
+        renderItem={(item) =>(
+            <List.Item>
+                <List.Item.Meta
+                avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+                title={<a href="https://ant.design">{item.title}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                />
+            </List.Item>
+    )} />
     )
 }
